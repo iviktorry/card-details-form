15b455a0dda07d76a68b54882414863b0941259f
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-21 12:51:47 +0200

    add bg image in App component for aside element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,15 @@
-export default function App() {}
+// import { bgMainMobile } from "../assets/bg-main-mobile.png";
+// import { bgMainDesktop } from "../assets/bg-main-desktop.png";
+
+export default function App() {
+  return (
+    <div className="min-h-lvh">
+      <div className="flex flex-col lg:flex-row">
+        <aside className="w-full h-60 bg-cover bg-center bg-[url('./assets/bg-main-mobile.png')] lg:h-screen lg:w-121 lg:bg-[url('./assets/bg-main-desktop.png')]"></aside>
+        <main>
+            
+        </main>
+      </div>
+    </div>
+  );
+}
