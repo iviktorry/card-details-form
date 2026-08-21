592c4d09d33d4ba6b5c3bf495deb29405cef13c8
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-21 22:02:37 +0200

    add cards imgs, add positioning

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
 export default function Button() {
   return (
-    <button type="submit" className="bg-purple-950 text-white py-4 rounded-lg tracking-wide">
+    <button
+      type="submit"
+      className="bg-purple-950 text-white py-4 rounded-lg tracking-wide"
+    >
       Confirm
     </button>
   );
