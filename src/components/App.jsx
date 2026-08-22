5a51a4d8a1d15a650db8a792be5a2b76bea00163
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-22 13:13:54 +0200

    fix styles to Cards comp, add SM breakpoint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,10 @@ import Form from "./Form";
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col font-sans font-medium lg:flex-row ">
-      <aside className="w-full h-60 bg-cover bg-center relative text-white bg-[url('./assets/bg-main-mobile.png')] lg:h-screen lg:w-121 lg:bg-[url('./assets/bg-main-desktop.png')]">
+      <aside className="w-full h-60 bg-cover flex justify-center bg-center relative text-white bg-[url('./assets/bg-main-mobile.png')] lg:h-screen lg:w-121 lg:bg-[url('./assets/bg-main-desktop.png')]">
         <Cards />
       </aside>
-      <main className="px-6 flex justify-center items-center flex-1 text-black my-15">
+      <main className="px-6 flex justify-center items-center flex-1 text-black my-[18%]">
         <Form />
       </main>
     </div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,28 +4,68 @@ import cardLogo from "../assets/card-logo.svg";
 
 export default function Cards() {
   return (
-    <>
-      <div className="w-4/5 max-h-40 max-w-70 z-1 absolute top-10 right-5">
-        <img className="" src={cardBack} alt="Card back side" />
-        <p className="absolute z-2 top-[44%] text-gray-200 right-10 text-xs">
-          000
-        </p>
+    <div className="mx-5 mt-7.5 h-50 relative max-w-93 sm:max-w-105">
+      <div className="absolute top-0 right-0 w-[85%] z-10">
+        <div className="relative">
+          <img className="w-full" src={cardBack} alt="Card back side" />
+          <p className="absolute z-30 top-[44%] right-[0%] text-right mr-8 text-gray-200 text-xs tracking-widest sm:right-[3%] sm:top-[45%]">
+            000
+          </p>
+        </div>
       </div>
-      <div className="absolute -bottom-13 left-5 z-4 w-4/5 max-h-40 max-w-70 ">
-        <img className="z-5" src={cardFront} alt="Card front side" />
-        <img
-          className="z-6 top-5 left-5 absolute max-h-7"
-          src={cardLogo}
-          alt="Card logo"
-        />
-        <div className="flex flex-col gap-2 justify-center bottom-18 relative px-5">
-          <p className=" z-6 text-lg tracking-widest  ">0000 0000 0000 0000</p>
-          <div className="flex justify-between">
+
+      <div className="relative bottom-0 left-0 w-[85%] z-20 mt-[27%]">
+        <div className="relative">
+          <img className="w-full" src={cardFront} alt="Card front side" />
+          <img
+            className="absolute top-[18%] left-[7%] h-[17%]"
+            src={cardLogo}
+            alt="Card logo"
+          />
+
+          <p className="absolute top-[54%] left-[7%] right-[4%] whitespace-nowrap tracking-widest overflow-hidden sm:text-2xl">
+            0000 0000 0000 0000
+          </p>
+          <div className="absolute bottom-[12%] left-[7%] right-[7%] flex justify-between">
             <p className="text-xs uppercase">John Doe</p>
             <p className="text-xs">00/00</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
+
+// export default function Cards() {
+//   return (
+//     <div className="mx-5 mt-10 h-42 relative max-w-72.5">
+//       <div className="absolute top-0 right-0 w-[72%] z-10">
+//         <div className="relative pt-[22%]">
+//           <img className="w-full" src={cardBack} alt="Card back side" />
+//           <p className="absolute bottom-[22%] right-[8%] text-right mr-8 text-gray-200 text-xs tracking-widest">
+//             000
+//           </p>
+//         </div>
+//       </div>
+
+//       <div className="relative bottom-0 left-0 w-[72%] z-20">
+//         <div className="relative">
+//           <img className="w-full" src={cardFront} alt="Card front side" />
+//           <img
+//             className="absolute top-[18%] left-[7%] h-[17%]"
+//             src={cardLogo}
+//             alt="Card logo"
+//           />
+
+//           <p className="absolute top-[54%] left-[7%] right-[4%] whitespace-nowrap overflow-hidden">
+//             0000 0000 0000 0000
+//           </p>
+//           <div className="absolute bottom-[12%] left-[7%] right-[7%] flex justify-between">
+//             <p className="text-xs uppercase">John Doe</p>
+//             <p className="text-xs">00/00</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
